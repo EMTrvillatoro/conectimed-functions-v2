a1147498040d52b1c130006cb949bd8f5b7ff3a7
Rolando Villatoro <rvillatoro@emt.dev>
2025-09-03 16:34:22 -0600

    feat: userAppRegister update pt9

--- functions/assets/js/user/register.js
+++ functions/assets/js/user/register.js
@@ -151,4 +151,123 @@ async function registerHandler(req, res) {
 
 }
 
-module.exports = { registerHandler };
+/**
+ * 
+ * @param { import('express').Request } req 
+ * @param { import('express').Response } res 
+ * @returns 
+ */
+
+async function updateHandler(req, res) {
+    res.set("Content-Type", "application/json");
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+
+    const admin = getFBAdminInstance();
+
+    const db = admin.firestore();
+
+    if (req.method === "OPTIONS") {
+        return res.status(204).send("");
+    }
+
+    if (req.method !== "PUT") {
+        return res.status(405).json({ code: 405, message: `${req.method} Method Not Allowed` });
+    }
+
+    try {
+
+        let { type, data, uid } = req.body;
+
+        if (!uid) {
+            console.warn("Faltan parámetros requeridos");
+            return res.status(400).json({ error: "Faltan parámetros requeridos" });
+        }
+
+        if (resp && resp.uid && resp.email) {
+            //DATOS DEL USUARIO
+            const nameStr = stringSearch(`${data?.lastName1 || ''} ${data?.lastName2 || ''} ${data?.name || ''}`, true);
+            let name = String(data?.name || '').trim().replace(/\s{2,}/g, ' ');
+            let lastName1 = String(data?.lastName1 || '').trim().replace(/\s{2,}/g, ' ');
+            let lastName2 = String(data?.lastName2 || '').trim().replace(/\s{2,}/g, ' ');
+            name = String(name).toLocaleLowerCase().replace(/\w\S*/g, w => w.replace(/^\w/, c => c.toUpperCase()));
+            lastName1 = String(lastName1).toLocaleLowerCase().replace(/\w\S*/g, w => w.replace(/^\w/, c => c.toUpperCase()));
+            lastName2 = String(lastName2).toLocaleLowerCase().replace(/\w\S*/g, w => w.replace(/^\w/, c => c.toUpperCase()));
+     
+            // Guardar el usuario en Firestore usando una transacción
+            const userRef = db.collection("users").doc(resp.uid);
+            const userMetaRef = db.collection("medico-meta").doc(resp.uid);
+
+            const estimatedGraduationTime = data?.estimatedGraduationTime || undefined;
+            const studentVerificationFileUrl = data?.studentVerificationFileUrl || undefined;
+
+           /*  await db.runTransaction(async (transaction) => {
+                let metadata = {
+                    specialty1: data?.specialty1 || null,
+                    specialty2: data?.specialty2 || null,
+                    specialty3: data?.specialty3 || null,
+                    specialty4: data?.specialty4 || null,
+                    specialty5: data?.specialty5 || null
+                }
+
+                const search = await updateUserSearch({ name, lastName1, lastName2, email: resp.email }, metadata);
+
+                let _data = {                    
+                    personalInterests: data?.personalInterests || [],
+                    search: search || [],
+                    updatedAt: FieldValue.serverTimestamp(),
+                }
+
+                if (estimatedGraduationTime !== undefined && studentVerificationFileUrl !== null) {
+                    _data.estimatedGraduationTime = estimatedGraduationTime;
+                    _data.studentVerificationFileUrl = studentVerificationFileUrl;
+                }
+
+                transaction.set(userRef, _data, { merge: true });
+
+                // Elimina todas las propiedades de metadata que sean === null
+                Object.keys(metadata).forEach(key => {
+                    if (metadata[key] == null) {
+                        delete metadata[key];
+                    }
+                });
+
+                transaction.set(userMetaRef, {
+                    ...metadata,
+                    cedulaProfesional: data?.cedula || '',
+                    address1: data?.address1 || {},
+                }, { merge: true });
+
+            }); */
+
+            // Obtener custom token para el usuario
+
+            return res.status(200).json({
+                status: "success",
+                message: "Usuario creado exitosamente",
+                user: {
+                    uid: resp.uid,
+                    email: resp.email
+                }
+            });
+
+        } else {
+            return res.status(400).json({
+                status: "error",
+                message: "No se pudo crear el usuario",
+                error: resp
+            });
+        }
+
+    } catch (e) {
+        console.error("Error en userAppRegister:", e);
+        return res.status(200).json({
+            message: e.message || "Error desconocido",
+            code: e.code || "unknown_error",
+            status: "error"
+        });
+    }
+
+}
+
+module.exports = { registerHandler, updateHandler };
--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const { stripeCustomerCreateHandler, stripeCustomerDeleteHandler, stripeCustomer
 const { infoDBFHandler } = require('./assets/js/experimental/experimental');
 const { getValidatedUsersHandler, getAllAuthUsersHandler } = require('./assets/js/members/createusers');
 const { clickMeetingHandler } = require('./assets/js/click-meeting/click-meeting');
-const { registerHandler } = require('./assets/js/user/register');
+const { registerHandler, updateHandler } = require('./assets/js/user/register');
 const { generatePDFHandler } = require('./assets/js/pdf/pdf');
 const { generateHtmlCertificateHandler } = require('./assets/js/pdf/html');
 const { sendMailHandler } = require('./assets/js/utils/sendMail');
@@ -13,22 +13,55 @@ const { getSpecialties, getSpecialty } = require('./assets/js/specialties/specia
 
 /* functions HTTP REQUEST */
 
-/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.infoDBF = onRequest(runtimeOpts, async (req, res) => await infoDBFHandler(req, res));
-/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.stripe_customer_create = onRequest(async (req, res) => await stripeCustomerCreateHandler(req, res));
-/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.stripe_customer_update = onRequest(runtimeOpts, async (req, res) => await stripeCustomerUpdateHandler(req, res));
-/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.stripe_customer_retrieve = onRequest(runtimeOpts, async (req, res) => await stripeCustomerRetrieveHandler(req, res));
-/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.stripe_customer_delete = onRequest(runtimeOpts, async (req, res) => await stripeCustomerDeleteHandler(req, res));
-/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.stripe_payment_intent = onRequest(runtimeOpts, async (req, res) => await stripePaymentIntentHandler(req, res));
-/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.stripe_payment_intent_update = onRequest(runtimeOpts, async (req, res) => await stripePaymentIntentUpdateHandler(req, res));
-/* DESC: GET URL FROM CLICK MEETING | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.clickMeeting = onRequest(async (req, res) => await clickMeetingHandler(req, res));
-/* DESC: GET VALIDATED USERS | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.getValidatedUsers = onRequest(runtimeOpts, async (req, res) => await getValidatedUsersHandler(req, res));
-/* DESC: USER APP REGISTRATION | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.userAppRegister = onRequest(runtimeOpts, async (req, res) => await registerHandler(req, res));
-/* DESC: GET ALL AUTH USERS | AUTHOR: Miguel | TYPE: HTTP REQUEST */ exports.getAllAuthUsers = onRequest(runtimeOpts, async (req, res) => await getAllAuthUsersHandler(req, res));
-/* DESC: GEN HTML CERT | AUTHOR: Miguel | TYPE: HTTP REQUEST */ exports.generateHtmlCertificate = onRequest(runtimeOpts, async (req, res) => await generateHtmlCertificateHandler(req, res));
-/* DESC: GEN PDF FILE | AUTHOR: Miguel | TYPE: HTTP REQUEST */ exports.generatePDF = onRequest(runtimeOpts, async (req, res) => await generatePDFHandler(req, res));
-/* DESC: GET ALL SPECIALTIES | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.getSpecialties = onRequest(runtimeOpts, async (req, res) => await getSpecialties(req, res));
-/* DESC: GET SPECIALTY BY ID | AUTHOR: Rolando | TYPE: HTTP REQUEST */ exports.getSpecialty = onRequest(runtimeOpts, async (req, res) => await getSpecialty(req, res));
+/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.infoDBF = onRequest(runtimeOpts, async (req, res) => await infoDBFHandler(req, res));
+
+/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.stripe_customer_create = onRequest(async (req, res) => await stripeCustomerCreateHandler(req, res));
+
+/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.stripe_customer_update = onRequest(runtimeOpts, async (req, res) => await stripeCustomerUpdateHandler(req, res));
+
+/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.stripe_customer_retrieve = onRequest(runtimeOpts, async (req, res) => await stripeCustomerRetrieveHandler(req, res));
+
+/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.stripe_customer_delete = onRequest(runtimeOpts, async (req, res) => await stripeCustomerDeleteHandler(req, res));
+
+/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.stripe_payment_intent = onRequest(runtimeOpts, async (req, res) => await stripePaymentIntentHandler(req, res));
+
+/* DESC: STRIPE | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.stripe_payment_intent_update = onRequest(runtimeOpts, async (req, res) => await stripePaymentIntentUpdateHandler(req, res));
+
+/* DESC: GET URL FROM CLICK MEETING | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.clickMeeting = onRequest(async (req, res) => await clickMeetingHandler(req, res));
+
+/* DESC: GET VALIDATED USERS | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.getValidatedUsers = onRequest(runtimeOpts, async (req, res) => await getValidatedUsersHandler(req, res));
+
+/* DESC: USER APP REGISTRATION | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.userAppRegister = onRequest(runtimeOpts, async (req, res) => await registerHandler(req, res));
+
+/* DESC: USER APP UPDATE | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.userAppUpdate = onRequest(runtimeOpts, async (req, res) => await updateHandler(req, res));
+
+/* DESC: GET ALL AUTH USERS | AUTHOR: Miguel | TYPE: HTTP REQUEST */
+exports.getAllAuthUsers = onRequest(runtimeOpts, async (req, res) => await getAllAuthUsersHandler(req, res));
+
+/* DESC: GEN HTML CERT | AUTHOR: Miguel | TYPE: HTTP REQUEST */
+exports.generateHtmlCertificate = onRequest(runtimeOpts, async (req, res) => await generateHtmlCertificateHandler(req, res));
+
+/* DESC: GEN PDF FILE | AUTHOR: Miguel | TYPE: HTTP REQUEST */
+exports.generatePDF = onRequest(runtimeOpts, async (req, res) => await generatePDFHandler(req, res));
+
+/* DESC: GET ALL SPECIALTIES | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.getSpecialties = onRequest(runtimeOpts, async (req, res) => await getSpecialties(req, res));
+
+/* DESC: GET SPECIALTY BY ID | AUTHOR: Rolando | TYPE: HTTP REQUEST */
+exports.getSpecialty = onRequest(runtimeOpts, async (req, res) => await getSpecialty(req, res));
 
 /* functions CALLABLES */
 
-/* DESC: SEND MAIL | AUTHOR: Miguel | TYPE: CALLABLE */exports.sendMail = onCall(async (data, context) => await sendMailHandler(data, context));
+/* DESC: SEND MAIL | AUTHOR: Miguel | TYPE: CALLABLE */
+exports.sendMail = onCall(async (data, context) => await sendMailHandler(data, context));
